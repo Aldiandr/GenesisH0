abae7e8fccdb137b0d4c03dd8ca554accb038b5a
Rtidcoin <56807547+RtidCoin@users.noreply.github.com>
2020-04-09 08:27:08 +0700

    Update genesis.py

--- genesis.py
+++ genesis.py
@@ -30,7 +30,7 @@ def get_args():
   parser.add_option("-n", "--nonce", dest="nonce", default=0,
                    type="int", help="the first value of the nonce that will be incremented when searching the genesis hash")
   parser.add_option("-a", "--algorithm", dest="algorithm", default="SHA256",
-                    help="the PoW algorithm: [SHA256|scrypt|X11|X13|X15]")
+                    help="the PoW algorithm: [SHA256|yespower|X11|X13|X15]")
   parser.add_option("-p", "--pubkey", dest="pubkey", default="04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f",
                    type="string", help="the pubkey found in the output script")
   parser.add_option("-v", "--value", dest="value", default=5000000000,
@@ -40,14 +40,14 @@ def get_args():
 
   (options, args) = parser.parse_args()
   if not options.bits:
-    if options.algorithm == "scrypt" or options.algorithm == "X11" or options.algorithm == "X13" or options.algorithm == "X15":
+    if options.algorithm == "yespower" or options.algorithm == "X11" or options.algorithm == "X13" or options.algorithm == "X15":
       options.bits = 0x1e0ffff0
     else:
       options.bits = 0x1d00ffff
   return options
 
 def get_algorithm(options):
-  supported_algorithms = ["SHA256", "scrypt", "X11", "X13", "X15"]
+  supported_algorithms = ["SHA256", "yespower", "X11", "X13", "X15"]
   if options.algorithm in supported_algorithms:
     return options.algorithm
   else:
@@ -143,7 +143,7 @@ def generate_hash(data_block, algorithm, start_nonce, bits):
 def generate_hashes_from_block(data_block, algorithm):
   sha256_hash = hashlib.sha256(hashlib.sha256(data_block).digest()).digest()[::-1]
   header_hash = ""
-  if algorithm == 'scrypt':
+  if algorithm == 'yespower':
     header_hash = scrypt.hash(data_block,data_block,1024,1,1,32)[::-1] 
   elif algorithm == 'SHA256':
     header_hash = sha256_hash
